d23ea245aa5e5f6847c07291ce2877dbfc608128
lvdinghao <lvdinghao@shanlinjinrong.com>
2018-01-16 12:48:25 +0800

    添加DingTestLib

---
+++ dingtestlib/src/main/java/ding/com/dingtestlib/utils/NumberUtils.java
@@ -0,0 +1,11 @@
+package ding.com.dingtestlib.utils;
+
+/**
+ * @author ding
+ * @date 1/16/18
+ */
+public class NumberUtils {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
